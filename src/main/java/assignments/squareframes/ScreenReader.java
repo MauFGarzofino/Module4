a3c2b4da18d3e1525551eeb103bbcfacd01f2a83
Mauricio Flores Garzofino <mauricioflores123321@gmail.com>
2024-03-21 09:34:38 -0400

    UpdatePC

--- src/main/java/assignments/squareframes/ScreenReader.java
+++ src/main/java/assignments/squareframes/ScreenReader.java
@@ -3,13 +3,14 @@ package assignments.squareframes;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ScreenReader {
     public static char[][] readScreenFromFile(String filePath) throws IOException {
         List<String> lines = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath, StandardCharsets.UTF_8))) {
             String line;
             while ((line = reader.readLine()) != null && !line.isEmpty()) {
                 lines.add(line);
